5fa84adf02756015cd8f28d75526cf516c5a12bf
Jk C <jkcoxson@gmail.com>
2021-07-19 00:11:25 -0600

    fix colors (#6)
    * Create readme
    
    * License
    
    * Add hotswap of servers
    
    * Add commands to change channels
    
    * Update README.md
    
    Co-authored-by: sb64 <53383020+sb64@users.noreply.github.com>
    
    * Make fixes for the Multiplexer
    
    * Missing end of code
    
    * Add functions for different packets
    
    * Add ready packet before sending commands
    
    * Re-add ready
    
    * Fix merge errors
    
    * Fix deprecation warnings
    
    * Fix colors for teams
    
    Co-authored-by: sb64 <53383020+sb64@users.noreply.github.com>

--- chatMessage.js
+++ chatMessage.js
@@ -3,16 +3,18 @@
 
 
 module.exports = class chatMessage {
-    constructor(message, sender, game) {
+    constructor(message, sender, color, game) {
         this.content = message;
         this.sender = sender;
         this.game = game;
         this.sendToDiscord = true;
         this.sendToMinecraft = true;
+        this.color = color;
     }
     content;
     sender;
     game;
     sendToDiscord;
     sendToMinecraft;
+    color;
 };
